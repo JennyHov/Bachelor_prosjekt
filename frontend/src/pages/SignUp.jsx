67c9a39790ea04e70232cf99adc658555c7ba1b7
Frederik A.Biltvedt <FredABHere@hotmail.com>
2024-03-07 09:53:45 +0100

    added sign up ui without css for http://localhost:5173/sign-up

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
     <div>
-      SignUp
+      <h1>Sign Up</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Username'
+          id='username'
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+        />
+        <button>
+          Sign up
+        </button>
+      </form>
+      <div>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
