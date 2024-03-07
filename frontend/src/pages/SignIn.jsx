428c33ac51e3f9d937e89bb98ce95921a2c7b8b4
Frederik A.Biltvedt <FredABHere@hotmail.com>
2024-03-07 12:08:06 +0100

    completed the sign in page functionality

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div>
-      SignIn
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          onChange={handleChange}
+        />
+        <button disabled={loading}>
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div>
+        <p>Don&#39;t have an account?</p>
+        <Link to='/sign-up'>
+          <span>Sign up</span>
+        </Link>
+      </div>
+      <p>{error && 'Something went wrong!'}</p>
     </div>
   )
 }
