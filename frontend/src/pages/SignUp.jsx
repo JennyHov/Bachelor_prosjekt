6787b31825338f4a30fc75706bb2dfa9d42c6382
Frederik A.Biltvedt <FredABHere@hotmail.com>
2024-03-07 10:43:35 +0100

    completed sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,27 +1,63 @@
+import { set } from 'mongoose';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           placeholder='Username'
           id='username'
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='Email'
           id='email'
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='Password'
           id='password'
+          onChange={handleChange}
         />
-        <button>
-          Sign up
+        <button disabled={loading}>
+          {loading ? 'Loading...' : 'Sign up'}
         </button>
       </form>
       <div>
@@ -30,6 +66,7 @@ export default function SignUp() {
           <span>Sign in</span>
         </Link>
       </div>
+      <p>{error && 'Something went wrong!'}</p>
     </div>
   )
 }
