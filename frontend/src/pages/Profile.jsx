dc5b89ce3f3e1e4c253bf9d7b411299db7576259
Frederik A.Biltvedt <FredABHere@hotmail.com>
2024-03-16 22:50:24 +0100

    complete upload functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,19 +1,63 @@
 import { useSelector } from 'react-redux'
 import React from 'react'
-
+import { useEffect, useState, useRef } from 'react';
+import { app } from '../firebase';
+import { getStorage, uploadBytesResumable, getDownloadURL, ref } from 'firebase/storage';
 
 
 export default function Profile() {
+  const [picture, setPicture] = useState(undefined);
+  const [picturePercentage, setPicturePercentage] = useState(0);
+  const referanceFile = useRef(null);
+  const [pictureError, setPictureError] = useState(false);
+  const [formData, setFormData] = useState({});
+  
   const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (picture) {
+      handlePictureUploading(picture);
+    }
+  }, [picture]);
+  const handlePictureUploading = async (picture) => {
+    const storage = getStorage(app);
+    const nameOfFile = new Date().getTime()+picture.name;
+    const referanceFromFirebase = ref(storage, nameOfFile);
+    const uploadingTask = uploadBytesResumable(referanceFromFirebase, picture);
+    uploadingTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setPicturePercentage(Math.round(progress));
+      },
+      (error) => {
+        setPictureError(true);
+      },
+      () => {
+        getDownloadURL(uploadingTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profileImage: downloadURL })
+        );
+      }
+    );
+  };
   return (
     <div>
     <h1>Profile</h1>
     <form>
+      <input type="file" ref={referanceFile} hidden accept="image/*" onChange={(e) => setPicture(e.target.files[0])}/>
       <img
         src={currentUser.profileImage}
         alt="profile"
         className=""
+        onClick={() => referanceFile.current.click()}
       />
+      <p>
+        {pictureError ? (<span>Error when uploading picture (file must be a pictuere and size must be less or equal to 2 MB)</span>
+        ) : picturePercentage > 0 && picturePercentage < 100 ? ( <span>{`Uploading in progress: ${picturePercentage}%`}</span>
+        ) : picturePercentage === 100 ? (<span>Picture uploaded successfully!</span>
+        ) : ( 
+          "" 
+        )}
+      </p>
       <input
         defaultValue={currentUser.username}
         type="text"
