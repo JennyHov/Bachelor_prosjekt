476885f2c9fab51f0697b6a0beaa4160373c1bb8
Frederik A.Biltvedt <FredABHere@hotmail.com>
2024-03-16 21:12:53 +0100

    completed ui for user's profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,45 @@
+import { useSelector } from 'react-redux'
 import React from 'react'
 
+
+
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div>
-      Profile
+    <h1>Profile</h1>
+    <form>
+      <img
+        src={currentUser.profileImage}
+        alt="profile"
+        className=""
+      />
+      <input
+        defaultValue={currentUser.username}
+        type="text"
+        id="username"
+        placeholder="username"
+        className=""
+      />
+      <input
+        defaultValue={currentUser.email}
+        type="email"
+        id="email"
+        placeholder="Email"
+        className=""
+      />
+      <input
+        type="password"
+        id="password"
+        placeholder="password"
+        className=""
+      />
+      <button>update</button>
+    </form>
+    <div>
+      <span>Delete Account</span>
+      <span>Sign out</span>
     </div>
-  )
+  </div>
+);
 }
