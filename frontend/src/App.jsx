1f1ed3890813f132fa73f9dfc523ea7b51dfd249
Jenny Hovland <jenny.hovland@hva.nl>
2024-04-08 11:15:09 +0200

    Copy pastet fra Mirdit sitt andre prosjekt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,34 @@
-import PrivateRoute from './components/PrivateRoute';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
+import React, { useState } from 'react';
+import reactLogo from '../../frontend/src/assets/react.svg';
+import viteLogo from '../public/vite.svg';
+import '../src/app.css';
 
+function App() {
 
-
-
-export default function App() {
   return (
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-
-        <Route element={<PrivateRoute />} >
-          <Route path="/user-profile" element={<Profile />} />
-        </Route>
-        
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
