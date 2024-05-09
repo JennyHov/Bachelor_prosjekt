7f6a6e8ce5215e545b63cfea47449279229d8bdd
Frederik <fredabhere@hotmail.com>
2024-05-09 03:59:43 +0200

    fikset litt på sign up siden så det er levelig

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Oauth from '../components/Oauth';
 import '../css/SignUp.css';
+import GoogleImage from '../../../assets/images/login/google.png';
+import LoginPopup from './LoginPopup'; // Sørg for at stien er korrekt basert på filstrukturen din
+
+
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [showLoginPopup, setShowLoginPopup] = useState(false); 
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -30,46 +35,70 @@ export default function SignUp() {
         setError(true);
         return;
       }
-      navigate('/sign-in');
+      setFormData({ fullName: '', email: '', password: '' });
+      setShowLoginPopup(true);
     } catch (error) {
       setLoading(false);
       setError(true);
     }
   };
+
+
+  const handleCloseLoginPopup = () => {
+    setShowLoginPopup(false);
+  };
+
   return (
     <div className="signup-container">
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Full Name'
-          id='fullName'
-          onChange={handleChange}
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          id='email'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          id='password'
-          onChange={handleChange}
-        />
-        <button disabled={loading}>
-          {loading ? 'Loading...' : 'Sign up'}
-        </button>
-        <Oauth />
-      </form>
-      <div>
-        <p>Have an account?</p>
-        <Link to='/sign-in'>
-          <span>Sign in</span>
-        </Link>
+      {showLoginPopup && (
+        <LoginPopup isOpen={showLoginPopup} onClose={handleCloseLoginPopup} />
+      )}
+      <div className='signup-content'>
+        <h1 className='sign-up-title'>Sign Up</h1>
+        <form onSubmit={handleSubmit} className='signup-form-container'>
+          <input
+            className='form-control signup-input'
+            type='text'
+            placeholder='Full Name'
+            id='fullName'
+            value={formData.fullName || ''}
+            onChange={handleChange}
+          />
+          <input
+            className='form-control signup-input'
+            type='email'
+            placeholder='Email'
+            id='email'
+            value={formData.email || ''}
+            onChange={handleChange}
+          />
+          <input
+            className='form-control signup-input'
+            type='password'
+            placeholder='Password'
+            id='password'
+            value={formData.password || ''}
+            onChange={handleChange}
+          />
+          <div className='button-container'>
+            <div>
+              <button disabled={loading} className='sign-in-btn'>
+              {loading ? 'Loading...' : 'Sign up'}
+              </button>
+            </div>
+            <div className='divider'>
+              <span className='divider-line'></span>
+              <span className='divider-text'>Or</span>
+              <span className='divider-line'></span>
+            </div>
+            <div className='google-login-btn' type='button'>
+              <img src={GoogleImage} alt="Google logo" className='google-logo'/>
+              <Oauth />
+            </div>
+          </div>
+          </form>
+          {error && 'Something went wrong!'}
       </div>
-      {error && 'Something went wrong!'}
     </div>
   )
 }
